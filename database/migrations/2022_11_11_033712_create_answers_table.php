d3569a37b79fd41f5b9612734729f467dff143ac
codeguna <pratamagunadhi@gmail.com>
2022-11-17 11:31:07 +0700

    Nullable on diagnoses

--- database/migrations/2022_11_11_033712_create_answers_table.php
+++ database/migrations/2022_11_11_033712_create_answers_table.php
@@ -16,27 +16,27 @@ class CreateAnswersTable extends Migration
         Schema::create('answers', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('id_diagnosis')->unsigned();
-            $table->boolean('g001');
-            $table->boolean('g002');
-            $table->boolean('g003');
-            $table->boolean('g004');
-            $table->boolean('g005');
-            $table->boolean('g006');
-            $table->boolean('g007');
-            $table->boolean('g008');
-            $table->boolean('g009');
-            $table->boolean('g010');
-            $table->boolean('g011');
-            $table->boolean('g012');
-            $table->boolean('g013');
-            $table->boolean('g014');
-            $table->boolean('g015');
-            $table->boolean('g016');
-            $table->boolean('g017');
-            $table->boolean('g018');
-            $table->boolean('g019');
-            $table->boolean('g020');
-            $table->boolean('g021');
+            $table->boolean('g001')->nullable();
+            $table->boolean('g002')->nullable();
+            $table->boolean('g003')->nullable();
+            $table->boolean('g004')->nullable();
+            $table->boolean('g005')->nullable();
+            $table->boolean('g006')->nullable();
+            $table->boolean('g007')->nullable();
+            $table->boolean('g008')->nullable();
+            $table->boolean('g009')->nullable();
+            $table->boolean('g010')->nullable();
+            $table->boolean('g011')->nullable();
+            $table->boolean('g012')->nullable();
+            $table->boolean('g013')->nullable();
+            $table->boolean('g014')->nullable();
+            $table->boolean('g015')->nullable();
+            $table->boolean('g016')->nullable();
+            $table->boolean('g017')->nullable();
+            $table->boolean('g018')->nullable();
+            $table->boolean('g019')->nullable();
+            $table->boolean('g020')->nullable();
+            $table->boolean('g021')->nullable();
             $table->timestamps();
 
             $table->foreign('id_diagnosis')
